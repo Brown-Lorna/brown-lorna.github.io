0d365bc877c2e3e96f6e098da8de896c46642ba6
Brown-Lorna <bro12070@byui.edu>
2019-07-03 17:43:06 -0400

    Updated Preston link in Gallery page. Changed 5-day forecast to display names of 5 days instead of just the first one.

--- assignments/lesson-10/scripts/forecastinfo.js
+++ assignments/lesson-10/scripts/forecastinfo.js
@@ -9,8 +9,18 @@ weatherObj2.onload = function() {
     showForecast(forecastObj);
 }
 
+function getDayofWeek(daynum) {
+    var days = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+    var dayofweek = days[daynum];
+    return dayofweek;
+}
+
+
 function showForecast(jsonObj) {
     var forecastStuff = jsonObj;
+
+    var d = new Date();
+    var daynum = d.getDay();
 
     for (var i = 0; i < forecastStuff.list.length; i++) {
         if (forecastStuff.list[i].dt_txt.includes("18:00:00")) {
@@ -19,23 +29,15 @@ function showForecast(jsonObj) {
             var myH4 = document.createElement("h4");
             var myImg = document.createElement("img");
             var myH5 = document.createElement("h5");
+            
+            if (daynum == 7) {
+                daynum = 0;
+            }
+            
+            var dayName = getDayofWeek(daynum);
+            daynum++;
 
-            // Area under construction
-            // Still need to get it to pull the right day from the array
-            // % of (day number / 8 )
-
-            // j=i++ if j > 6  else J-7
-            // for (var i = 0; i < 6; i++) {
-            //     var d = getDay(forecastStuff.list[i].dt_txt.includes("18:00:00"));
-
-            // }
-
-            // Placeholder during construction
-            var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-            myH4.textContent = (days[d.getDay(forecastStuff.list[i].dt_txt)]);
-
-            // End of area under construction
-
+            myH4.textContent = dayName;
             myImg.setAttribute('src', "http://openweathermap.org/img/wn/" + forecastStuff.list[i].weather[0].icon + ".png");
             myImg.setAttribute('alt', 'icon for ' + forecastStuff.list[i].weather.description + 'weather');
             myH5.textContent = (forecastStuff.list[i].main.temp + " \xB0" + "F");
@@ -75,3 +77,30 @@ function showForecast(jsonObj) {
 // var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
 // document.getElementById("dayofweek").innerHTML = (days[d.getDay()])  
+
+            // Area under construction
+            // Still need to get it to pull the right day from the array
+            // % of (day number / 8 )
+
+            // j=i++ if j < 6  else J-7
+            // for (var i = 0; i < 6; i++) {
+            //     var d = getDay(forecastStuff.list[i].dt_txt.includes("18:00:00"));
+
+            // }
+            // var dayofweek = new array(5);
+
+            // );
+            // var d = forecastStuff.list[i].dt_txt;includes("18:00:00")
+            // var k = d.getDay();
+            // var x = weekday;
+
+            // if (k < 7)
+            // x = k;
+            // else {
+            //     x = k-7;
+            // }
+
+ 
+            // Placeholder during construction
+            // var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+            // dayofweek[i] = (days[d.getDay(forecastStuff.list[i].dt_txt)]);
